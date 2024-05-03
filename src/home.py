2ae9e15c2ad864eea9ccf43f2fd2a6164cddf368
yoyoyo-momo <bolinll2@illinois.edu>
2024-05-03 17:41:45 -0500

    update home

--- src/home.py
+++ src/home.py
@@ -4,28 +4,9 @@ import json
 
 def home():
     st.title('WHATNEXT')
-<<<<<<< Updated upstream
-    st.header('There are 4 ways to get recommendations:')
-
-    st.subheader('1. Prompt Recommendation')
-    st.text('In put your prompt here, and we will recommend songs for you!')
-    st.text_input('Enter your prompt here:')
-    st.button('Get prompt Recommendations')
-
-    st.subheader('2. Feature Recommendation')
-    danceability = st.slider('Danceability', min_value=0.0, max_value=1.0, value=0.5)
-    energy = st.slider('Energy')
-    valence = st.slider('Valence')
-    mode = st.slider('Mode')
-
-    
-=======
     st.header('3 ways to get recommendations:')
 
     st.subheader(':books: **Prompt Recommendation**')
-    prompt_options = st.multiselect(
-        "Please describe your feeling or feature you want in the song:",
-        ["Happy", "Sorrow", "Hype", "Blue"])
     
     age = st.slider("Your Age:", min_value=0, max_value=100, value=25)
     location = st.text_input("Location:", "Gym")
@@ -35,7 +16,6 @@ def home():
     data = {"age":age, "location":location, "mood":mood, "genre":genre, "requirement":requirement}
     with open('data.json', 'w') as file:
         json.dump(data, file)    
->>>>>>> Stashed changes
 
     st.subheader('3. Playlist Recommendation')
     st.text_input('Enter your Playlist URL here:')
