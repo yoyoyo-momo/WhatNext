eab7760b21b7b1800e3a161aa58227b3c740f6bb
shangrex <hshangrex@gmail.com>
2024-05-03 16:58:08 -0500

    Update home page

--- src/home.py
+++ src/home.py
@@ -3,26 +3,37 @@ import streamlit as st
 
 def home():
     st.title('WHATNEXT')
-    st.header('There are 4 ways to get recommendations:')
+    st.header('3 ways to get recommendations:')
 
-    st.subheader('1. Prompt Recommendation')
+    st.subheader(':books: **Prompt Recommendation**')
+    prompt_options = st.multiselect(
+        "Please describe your feeling or feature you want in the song:",
+        ["Happy", "Sorrow", "Hype", "Blue"])
+    
+    prompt_text = ""
+    if prompt_options:
+        prompt_text = "Give me some music that makes me feel "
+    for i in prompt_options:
+        prompt_text += i + " "
+
     st.text('In put your prompt here, and we will recommend songs for you!')
-    text_input = st.text_input('Enter your prompt here:')
+    text_area = st.text_area('Enter your prompt here:', 
+                             prompt_text)
     st.button('Get Prompt Recommendations')
 
-    st.subheader('2. Feature Recommendation')
-    danceability = st.slider('Danceability', min_value=0.0, max_value=1.0, value=0.5)
-    energy = st.slider('Energy', min_value=0.0, max_value=1.0, value=0.5)
-    valence = st.slider('Valence', min_value=0.0, max_value=1.0, value=0.5)
-    mode = st.slider('Mode', min_value=0.0, max_value=1.0, value=0.5)
-    st.button('Get Feature Recommendations')
+    # st.subheader('2. Feature Recommendation')
+    # danceability = st.slider('Danceability', min_value=0.0, max_value=1.0, value=0.5)
+    # energy = st.slider('Energy', min_value=0.0, max_value=1.0, value=0.5)
+    # valence = st.slider('Valence', min_value=0.0, max_value=1.0, value=0.5)
+    # mode = st.slider('Mode', min_value=0.0, max_value=1.0, value=0.5)
+    # st.button('Get Feature Recommendations')
 
     
 
-    st.subheader('3. Playlist Recommendation')
+    st.subheader(':notes: **Playlist Recommendation**')
     st.text_input('Enter your Playlist URL here:')
     st.button('Get Playlist Recommendations')
 
-    st.subheader('4. Song Recommendation')
+    st.subheader(':musical_score: **Song Recommendation**')
     st.text_input('Enter your Song URL here:')
     st.button('Get Song Recommendations')
