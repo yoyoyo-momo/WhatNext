c95b9edd00e5211c3e6b1b6696a7ac0c50be7d4f
yoyoyo-momo <bolinll2@illinois.edu>
2024-05-03 17:38:02 -0500

    update home

--- src/home.py
+++ src/home.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import json
 
 
 def home():
     st.title('WHATNEXT')
+<<<<<<< Updated upstream
     st.header('There are 4 ways to get recommendations:')
 
     st.subheader('1. Prompt Recommendation')
@@ -17,6 +19,23 @@ def home():
     mode = st.slider('Mode')
 
     
+=======
+    st.header('3 ways to get recommendations:')
+
+    st.subheader(':books: **Prompt Recommendation**')
+    prompt_options = st.multiselect(
+        "Please describe your feeling or feature you want in the song:",
+        ["Happy", "Sorrow", "Hype", "Blue"])
+    
+    age = st.slider("Your Age:", min_value=0, max_value=100, value=25)
+    location = st.text_input("Location:", "Gym")
+    mood = st.text_input("Your Current Mood:", "Happy")
+    genre = st.text_input("Genre:", "Pop")
+    requirement = st.text_input("Specific Requirements:", "I love pop music.")
+    data = {"age":age, "location":location, "mood":mood, "genre":genre, "requirement":requirement}
+    with open('data.json', 'w') as file:
+        json.dump(data, file)    
+>>>>>>> Stashed changes
 
     st.subheader('3. Playlist Recommendation')
     st.text_input('Enter your Playlist URL here:')
