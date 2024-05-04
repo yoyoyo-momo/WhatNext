858ac4410c9e1c285dfe4c2503047318412730e7
yoyoyo-momo <bolinll2@illinois.edu>
2024-05-03 19:46:46 -0500

    Update about.py

--- src/about.py
+++ src/about.py
@@ -4,21 +4,20 @@ def about():
     st.subheader('Prompt Recommendation Tutorial')
     st.markdown("""
                 1. Type in your information and press "Enter".
-                2. After you finish, click on "Get Recommendations Through Prompt".
+                2. After you finish, click on "Get Prompt Recommendations".
                 2. See your recommendations in the "Recommendations" page.
                 """)
-    st.video('Test_video.webm', loop=True, autoplay=True, muted=True)
 
-    st.subheader('Playlist URL Explanation')
+    st.subheader('Playlist URL Tutorial')
     st.markdown("""
-    1. Copy your personal playlist url and paste it in the column.
-    2. Press "Get playlist Recommendations".
+    1. Copy and paste your personal playlist url.
+    2. Press "Get Playlist Recommendations".
     3. See the generated playlist in the "Recommendations" page. 
     """)
 
-    st.subheader('Song URL Explanation')
+    st.subheader('Song URL Tutorial')
     st.markdown("""
-    1. Copy your own song url and paste it in the column.
-    2. Press "Get song Recommendations".
+    1. Copy and paste your own song url.
+    2. Press "Get Song Recommendations".
     3. See the generated playlist in the "Recommendations" page. 
     """)
