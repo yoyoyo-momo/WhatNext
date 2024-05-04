4cc773664d300820a639fad0ac902fa38e831e4f
sbytony <zhps500126@gmail.com>
2024-05-03 20:25:50 -0500

    Finish home, model, recommendations

--- src/recommendations.py
+++ src/recommendations.py
@@ -1,7 +1,8 @@
 import streamlit as st
-from model import song_model, playlist_model
+from model import song_model, playlist_model, prompt_model
 import streamlit.components.v1 as components
 from time import sleep
+
 
 def recommendations():
     st.title('WHATNEXT')
@@ -13,19 +14,22 @@ def recommendations():
     print(type(st.session_state.result_list))
 
     iteration = 0
-    for index, key in st.session_state.result_list.items():
+    for index, key in enumerate(st.session_state.result_list):
         display_url = "https://open.spotify.com/embed/track/" + key + "?utm_source=generator&theme=0"
-        print(display_url)
         components.iframe(display_url, width=700, height=100)
-        
+
         iteration += 1
         if iteration % 5 == 0:
             sleep(1)
 
 
 def song_recommendations():
-    st.session_state.result_list, error_msg = song_model(st.session_state.song_url, model="Model 1")
+    st.session_state.result_list, error_msg = song_model(st.session_state.song_url, model="Spotify Model")
 
 
 def playlist_recommendations():
-    st.session_state.result_list, error_msg = playlist_model(st.session_state.playlist_url, model="Model 1")
+    st.session_state.result_list, error_msg = playlist_model(st.session_state.playlist_url, model="Spotify Model")
+
+
+def prompt_recommendations():
+    st.session_state.result_list, error_msg = prompt_model()
